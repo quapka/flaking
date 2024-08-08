513619bda8bd9f59b855e16f58379cd013f4f2b3
Jan Kvapil <>
2024-08-08 13:08:35 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Testing Flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec { buildInputs = [ libgpg-error ]; };
+      }
+    );
+}
